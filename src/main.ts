b864930ac3ad6c42fbd6ad034be67f75f2c3e3b4
Nick Breslin <nickbreslin@gmail.com>
2023-05-24 23:06:09 -0500

    Setting up router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+import router from "./router";
 
 import { createPinia } from "pinia";
 const pinia = createPinia();
 
 import BoilerplatePlugin from "./plugins/BoilerplatePlugin";
 
+import './style.css'
+import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).use(pinia).use(BoilerplatePlugin).mount('#app')
+
+createApp(App).use(pinia).use(BoilerplatePlugin).use(router).mount('#app')
