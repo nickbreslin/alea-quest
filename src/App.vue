9f3eef0b53d767b934d957cdacc9e349e0ecce93
Nick Breslin <nickbreslin@gmail.com>
2023-05-24 22:24:14 -0500

    Adding pinia

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <font-awesome-icon :icon="['fas', 'dice-two']" />
 </template>
 
 <style scoped>
